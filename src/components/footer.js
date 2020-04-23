b36d99ed213787c9993a2d8791b4d69cbc18d9a7
ankurjaiswal496 <ankurjaiswal496@gmail.com>
2020-04-23 20:50:11 +0530

    first

---
+++ src/components/footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "./footer.css";
+
+const Footer = () => (
+  <footer className="footer">
+      <b>Developer=>ANKUR   &nbsp;  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            ContactInfo:<a href='https://ankurjaiswal496.github.io/ankur/' target='blank'>Click here</a>&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; OPEN SOURCE    </b>
+      
+      
+   
+  </footer>
+);
+
+
+
+export default Footer
+
+
