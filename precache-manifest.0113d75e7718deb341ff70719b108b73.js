8f71969312d811d4cac04e63f4ea70c0ec58a9ae
ankurjaiswal496 <ankurjaiswal496@gmail.com>
2020-04-23 21:01:35 +0530

    Updates

---
+++ precache-manifest.0113d75e7718deb341ff70719b108b73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b291cfb5e593ffbd26fe1de3b840df4f",
+    "url": "/go/index.html"
+  },
+  {
+    "revision": "9c9bbd591cca3529f44f",
+    "url": "/go/static/css/main.66bb13e7.chunk.css"
+  },
+  {
+    "revision": "da3bd07f0236d88c01f6",
+    "url": "/go/static/js/2.88491ff5.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/go/static/js/2.88491ff5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c9bbd591cca3529f44f",
+    "url": "/go/static/js/main.50fc4125.chunk.js"
+  },
+  {
+    "revision": "defa0c1337e478425c1e",
+    "url": "/go/static/js/runtime-main.59e8651c.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/go/static/media/image.d7265326.png"
+  }
+]);
